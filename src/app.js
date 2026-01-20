97ece20b7fb71edd95c65b20a139349678117d0a
rodriguezmontillajose39-dotcom <rodriguezmontillajose39@gmail.com>
2026-01-20 21:46:34 +0000

    <Random Cards>

--- src/app.js
+++ src/app.js
@@ -1,11 +1,60 @@
-import "bootstrap";
-import "./style.css";
+const Container = document.querySelector(".contenedor")
+const card = document.getElementById ("card")
+let topSuit = document.querySelector(".top-suit")
+let number = document.querySelector(".number")
+let buttomSuit = document.querySelector(".bottom-suit")
+const button = document.getElementById("BtnGenerar")
+const RemoveButton = document.getElementById("BtnRemove")
 
+const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+const suits = [
+    { symbol: '♠', color: "negro" },
+    { symbol: '♣', color: "negro" },
+    { symbol: '♥', color: "rojo"},
+    { symbol: '♦', color: "rojo"}
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+button.addEventListener("click", function(){
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
+
+const RandomSuitArr = getRandomNumber(suits.length)
+const RandomSuit = suits[RandomSuitArr]
+const RandomValuesArr = getRandomNumber(values.length)
+const RandomValue = values[RandomValuesArr]
+RenderCards(RandomSuit,RandomValue);
+
+function RenderCards (suit,value) { 
+    /*topSuit.textContent = suit.symbol
+    buttomSuit.textContent = suit.symbol
+    number.textContent = value
+    card.className = "card"
+    card.classList.add(suit.color)*/ 
+
+    const NewCard = document.createElement("div")
+    NewCard.className = "card"
+    NewCard.classList.add(suit.color)
+    NewCard.innerHTML += `
+        <div class="top-suit">${suit.symbol}</div>
+        <div class="number">${value}</div>
+        <div class="bottom-suit">${suit.symbol}</div>`
+    
+    Container.appendChild(NewCard)   
+}
+const AllCards = Container.querySelectorAll(".card")
+if(AllCards.length === 8 ){
+        alert("Para de generar cartas plis :)")
+    }
+})
+
+RemoveButton.addEventListener("click", function(){
+    
+    const LastCard = Container.lastElementChild;
+
+    if(LastCard){
+        LastCard.remove()  
+    }
+    
+})
